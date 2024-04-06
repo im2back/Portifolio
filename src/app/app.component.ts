96a7d3a4a658499bd2a917922459ef33ea544d65
Jefferson Richards <jefferson.richards@outlook.com>
2024-04-06 12:55:43 -0300

    organizando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './homepage/footer/footer.component';
+import { CabecalhoComponent } from './homepage/cabecalho/cabecalho.component';
+import { MenuComponent } from './homepage/menu/menu.component';
+import { DescricaoComponent } from './homepage/descricao/descricao.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,BadgeModule,ButtonModule,FooterComponent ],
+  imports: [RouterOutlet,BadgeModule,ButtonModule,FooterComponent,CabecalhoComponent,MenuComponent,DescricaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
