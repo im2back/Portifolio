edd5926c960d3b385fd89b70671e470814e28df5
Jefferson Richards <jefferson.richards@outlook.com>
2024-04-06 00:05:38 -0300

    Portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BadgeModule } from 'primeng/badge';
+import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BadgeModule,ButtonModule,FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
