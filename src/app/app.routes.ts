323519ab3894e948c2a0ad01c0b06e1f11120fb7
Jefferson Richards <jefferson.richards@outlook.com>
2024-04-07 09:28:52 -0300

    config route and adjustments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponentComponent } from './homepage/home-component/home-component.component';
+import { FotosComponent } from './certificados/fotos/fotos.component';
 
 export const routes: Routes = [
 
  {
   path: 'home',
   component: HomeComponentComponent
+ },
+ {
+  path: 'certificados',
+  component: FotosComponent
  }
 
 ];
--- src/app/certificados/fotos/fotos.component.ts
+++ src/app/certificados/fotos/fotos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-fotos',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './fotos.component.html',
   styleUrl: './fotos.component.css'
 })
